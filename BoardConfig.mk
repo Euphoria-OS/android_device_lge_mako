6e1b6dc16c652269981b2ffa86bdc281dac6bcbe
lion0738 <lion0738@naver.com>
2015-01-12 12:27:32 +0900

    mako: Add sepolicy for hardware tunables
    credits to myfluxi,
    https://github.com/CyanogenMod/android_device_lge_hammerhead/commit/d46d2487f65fa6c73a02dda3d78d386c9238449f
    
    Change-Id: Ic697bec0358cde6e622ab569a409fb0894e5ebb4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,7 @@ BOARD_SEPOLICY_UNION += \
        rmt.te \
        sensors.te \
        surfaceflinger.te \
+       system_app.te \
        system_server.te \
        tee.te \
        te_macros \
