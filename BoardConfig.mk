669a93dd5437f36ba7be306b92a73ada1216e89c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-03 11:02:57 +0000

    Mako: use eos defconfig
    Change-Id: Ic2712805ca3f1730ff588283eeb58011fa5af323

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 TARGET_KERNEL_SOURCE := kernel/lge/mako
-TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CONFIG := eos_mako_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
